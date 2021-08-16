0f7122557a45b49697c7b035caf9153cc774871c
justin <games.j@live.nl>
2021-08-16 17:43:31 +0200

    cleaned up files
    cleaned up files

---
+++ src/Component/MainContainer.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+
+class MainContainer extends Component{
+    render(){
+        return(
+           <div className="container">
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+               <p> HALLO THIS A IS A CONTAINER COMPONENT</p>
+           </div>
+        )
+    }
+}
+
+export default MainContainer
